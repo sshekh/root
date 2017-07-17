03e584575ad965858c9fdf5d8b3f684f7601e1a8
Saurav Shekhar <sauravshekhar01@gmail.com>
2017-07-17 13:47:22 +0200

    Changing indentation

--- tmva/tmva/test/DNN/RNN/TestForwardPass.cxx
+++ tmva/tmva/test/DNN/RNN/TestForwardPass.cxx
@@ -2,15 +2,15 @@
 // Author: Saurav Shekhar 22/06/17
 
 /*************************************************************************
- * Copyright (C) 2017, Saurav Shekhar                                    *
- * All rights reserved.                                                  *
- *                                                                       *
- * For the licensing terms see $ROOTSYS/LICENSE.                         *
- * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ * Copyright (C) 2017, Saurav Shekhar                                                      *
+ * All rights reserved.                                                                          *
+ *                                                                                                        *
+ * For the licensing terms see $ROOTSYS/LICENSE.                                     *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.                   *
  *************************************************************************/
 
 ////////////////////////////////////////////////////////////////////
-//Testing RNNLayer forward pass for Reference implementation      //
+//Testing RNNLayer forward pass for Reference implementation         //
 ////////////////////////////////////////////////////////////////////
 
 #include <iostream>
@@ -24,14 +24,14 @@ using namespace TMVA::DNN::RNN;
 
 //TEST(RNNTest, ForwardPass)
 //{
-//  EXPECT_EQ(testForwardPass<TReference<double>>(3, 8, 100, 50), 0.0);
+// EXPECT_EQ(testForwardPass<TReference<double>>(3, 8, 100, 50), 0.0);
 //}
 
 int main() {
-  std::cout << "Testing RNN Forward pass\n";
+   std::cout << "Testing RNN Forward pass\n";
 
-  std::cout << testForwardPass<TReference<double>>(1, 8, 100, 50)  << "\n";
-  std::cout << testForwardPass<TReference<double>>(5, 9, 128, 64)  << "\n";
+   std::cout << testForwardPass<TReference<double>>(1, 8, 100, 50)  << "\n";
+   std::cout << testForwardPass<TReference<double>>(5, 9, 128, 64)  << "\n";
 
-  return 0;
+   return 0;
 }
